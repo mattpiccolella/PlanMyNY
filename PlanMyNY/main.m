fee7018e54a8665a6fc36425797d6f9dc70eeff3
mjp2220 <mjp2220@columbia.edu>
2014-03-20 10:44:36 -0400

    Initial commit of PlanMyNY iPhone app

---
+++ PlanMyNY/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PlanMyNY
+//
+//  Created by Matt on 3/20/14.
+//  Copyright (c) 2014 Matthew Piccolella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
